83f7decd59aad1d3fc12b86e223267320f12b3b3
윤수영 <yunsuyeong@yunsuyeong-ui-MacBookAir.local>
2023-04-17 00:24:46 +0900

    2023/04/17(File Upload 2)

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,13 +1,19 @@
 import client from "../../client";
 import bycrpt from "bcrypt";
 import { protectResolver } from "../users.utils";
+import { createWriteStream } from "fs";
 
 const resolverfn = async (
   _,
   { firstName, lastName, username, email, password, bio, avatar },
   { loggedUser }
 ) => {
-  console.log(avatar);
+  const { filename, createReadStream } = await avatar;
+  const readStream = createReadStream();
+  const writeStream = createWriteStream(
+    `${process.cwd()}/uploads/${Date.now()}-${filename}`
+  );
+  readStream.pipe(writeStream);
   let hashed = null;
   if (password) {
     hashed = await bycrpt.hash(password, 10);
