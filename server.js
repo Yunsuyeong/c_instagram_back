5fd6253487ac216b65353dcb0b6d970e28cbd9c4
윤수영 <yunsuyeong@yunsuyeong-ui-MacBookAir.local>
2023-05-02 13:20:09 +0900

    2023/05/02(DM Update)

--- server.js
+++ server.js
@@ -1,19 +1,41 @@
 import "dotenv/config";
+import http from "http";
 import express from "express";
 import logger from "morgan";
 import { ApolloServer } from "apollo-server-express";
 import { getUser } from "./users/users.utils";
 import { typeDefs, resolvers } from "./schema";
+import pubsub from "./pubsub";
 
 const PORT = process.env.PORT;
 
 const apollo = new ApolloServer({
   resolvers,
   typeDefs,
-  context: async ({ req }) => {
-    return {
-      loggedUser: await getUser(req.headers.token),
-    };
+  context: async (ctx) => {
+    if (ctx.req) {
+      return {
+        loggedUser: await getUser(ctx.req.headers.token),
+      };
+    } else {
+      const {
+        connection: { context },
+      } = ctx;
+      return {
+        loggedUser: context.loggedUser,
+      };
+    }
+  },
+  subscriptions: {
+    onConnect: async ({ token }) => {
+      if (!token) {
+        throw new Error("You can't listen");
+      }
+      const loggedUser = await getUser(token);
+      return {
+        loggedUser,
+      };
+    },
   },
 });
 
@@ -22,6 +44,9 @@ app.use(logger("tiny"));
 app.use("/static", express.static("uploads"));
 apollo.applyMiddleware({ app });
 
-app.listen({ port: PORT }, () => {
+const httpServer = http.createServer(app);
+apollo.installSubscriptionHandlers(httpServer);
+
+httpServer.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}/graphql`);
 });
