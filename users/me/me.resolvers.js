23ebe9408997409c8f2065a0be22b4e5bafa1c41
윤수영 <yunsuyeong@yunsuyeong-ui-MacBookAir.local>
2023-05-06 16:33:48 +0900

    2023/05/06(Identification Function)

---
+++ users/me/me.resolvers.js
@@ -0,0 +1,10 @@
+import client from "../../client";
+import { protectedResolver } from "../../users/users.utils";
+
+export default {
+  Query: {
+    me: protectedResolver((_, __, { loggedUser }) =>
+      client.user.findUnique({ where: { id: loggedUser.id } })
+    ),
+  },
+};
